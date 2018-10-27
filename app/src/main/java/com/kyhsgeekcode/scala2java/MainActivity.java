16a0265867283057a26e5aa21d2e3c79e4eb6db3
root <root@localhost>
2018-10-28 00:31:14 +0900

    Initial commit

---
+++ app/src/main/java/com/kyhsgeekcode/scala2java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kyhsgeekcode.scala2java;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
